b93e32567c624682e8de07411f7a78409ce03de4
cuberolopez96 <cuberolopez96@gmail.com>
2016-05-17 10:01:06 +0200

    jquery listo faltan retoques finales margen entre las imagenes y flecha

--- js/main.js
+++ js/main.js
@@ -1,15 +1,55 @@
 (function(){
-    var issoy=false;
-    var isconocimientos= false;
-    $(document).ready(function(){
-        $('#soy').click(function(){
-            if(issoy == false){
-                $('#yosoy').show(1000);
-                issoy=true;
-            }else{
-                 $('#yosoy').hide(1000);
-                issoy=false;
-            }
+   $(document).ready(function(){
+       
+       if(window.innerWidth <= 650){
+           $('#misaptitudes div').hide(500);
+        $('#misconocimientos div').hide(500);
+        $('#misredes div').hide(500);
+        $('#misconocimientos header ').mouseenter(function(){
+            $('#misconocimientos div').show(1000);
+            $('#yosoy div').hide(1000);
+            $('#misaptitudes div').hide(1000);
+            $('#misredes div').hide(1000);
         })
-    });
+        $('#misaptitudes header').mouseenter(function(){
+            $('#misconocimientos div').hide(1000);
+            $('#misaptitudes div').show(1000);
+            $('#misredes div').hide(1000);
+            $('#yosoy div').hide(1000);
+        });
+         $('#misredes header').mouseenter(function(){
+            $('#yosoy  div').hide(1000); 
+            $('#misredes div').show(1000);
+            $('#misconocimientos div').hide(1000);
+            $('#misaptitudes div').hide(1000); 
+        });
+        $('#yosoy header').mouseenter(function(){
+           $('#yosoy div').show(1000);
+            $('#misconocimientos div').hide(1000);
+            $('#misaptitudes div').hide(1000);
+            $('#misredes div').hide(1000);
+            
+        });
+        
+       }else{
+          
+           $('#titulo #logo').show(1000).delay(1000);
+            $('main').mouseenter(function(){
+                $('#yosoy').show(1000)
+                setTimeout(function(){
+                    $('#misconocimientos').show(1000).delay(1000);
+                },1000);
+                setTimeout(function(){
+                    $('#misaptitudes').show(1000).delay(1000);
+                },2000);
+                setTimeout(function(){
+                    $('#misredes').show(1000).delay(1000);
+                },3000);
+                
+                
+                
+            })
+       }
+   })
+    
 })()
